da43dbce4b4b9043d7431c865a700e843364c7f5
Jordan Nelson <jnelsontn@gmail.com>
2017-01-26 15:56:12 -0600

    code for message

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,24 +2,9 @@ var messageField = document.getElementById("inputText");
 var clearButton = document.getElementById("clear-board-button");
 var darkCheck = document.getElementById("dark-theme-checkbox");
 var largeCheck = document.getElementById("large-text-checkbox");
-
+/*
 messageField.addEventListener("keypress", submitMessage);
 clearButton.addEventListener("click", clearBoard);
 darkCheck.addEventListener("change", makeDark);
 largeCheck = document.getElementById("change", makeLarge);
-
-
-submitMessage(event) {};
-
-clearBoard() {};
-
-var MessageBoard = (function() {
-
-  // Private variable to store the price
-  
-
-  // Return the public interface that other code can interact with
-  return {
-    }
-  };
-})();
+*/
--- javascript/message.js
+++ javascript/message.js
@@ -1,7 +1,52 @@
+// One IIFE should contain a function that accepts an element id, 
+// and the user message, and then add the user's message - along with the delete button - 
+// to the specified parent element. Each message should be stored in a private array in this IIFE. 
+// This IIFE should also expose a function to read all messages, and delete a single message.
 
-var MessageBoard = (function(message) {
+var MessageBoard = (function(originalMessageBoard) {
 
+	// 1.  Create an array for the messages
+	var messageLog = [];
+
+	// 2. Event Listener for enter button grabs .value of input field
+	var inputField = document.getElementById("inputText");
+	inputField.addEventListener("keypress", function(event){
+		if (event.keycode === 13) {
+			console.log("eat");
+			var messageText = inputField.value;
+			messageLog.push(messageText);
+			console.log(messageLog);
+			MessageBoard.addMessage(messageText);	
+		}
+	});
+
+ // Expose a function to read all messages and delete a single message
+	  return {
+	  	readMessage: function(x) {
+	    },
+	    addMessage: function(potatoe) {
+	    	var messageList = document.getElementById("message-board-list");
+	    	var createLi = document.createElement("LI");
+	    	var theMessage = document.createTextNode(potatoe);
+
+	    	var deleteButton = document.createElement("BUTTON");
+	    	deleteButton.id = potatoe;
+
+	    	createLi.appendChild(theMessage);
+	    	createLi.appendChild(deleteButton);
+	    	messageList.appendChild(createLi);
+
+	    }
+	  };
 
   // Return the new, augmented object with the new method on it
-  return message;
-})(MessageBoard);
+  return originalMessageBoard;
+
+})(MessageBoard || {});
+
+
+/* valueButton.id = "textAreaButton";
+var buttonText = document.createTextNode("Create a Card");
+valueButton.appendChild(buttonText);
+document.body.appendChild(valueButton);
+*/
