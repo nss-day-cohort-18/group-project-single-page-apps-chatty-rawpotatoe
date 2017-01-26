6a60b700a3d48180a7b13d368ef033cb56eff10b
Jeremy Bakker <jeremybakker@Jeremys-MacBook-Pro.local>
2017-01-26 14:28:02 -0600

    added event listener template

---
+++ javascript/javascript.js
@@ -0,0 +1,15 @@
+var messageField = document.getElementById("inputText");
+var clearButton = document.getElementById("clear-board-button");
+var darkCheck = document.getElementById("dark-theme-checkbox");
+var largeCheck = document.getElementById("large-text-checkbox");
+
+messageField.addEventListener("keypress", submitMessage);
+clearButton.addEventListener("click", clearBoard);
+darkCheck.addEventListener("change", makeDark);
+largeCheck = document.getElementById("change", makeLarge);
+
+
+submitMessage(event) {};
+
+clearBoard() {};
+
