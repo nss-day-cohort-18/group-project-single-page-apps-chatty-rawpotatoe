2cdf19dd296b0d385d330d695e7ed0606c699d5f
Adam Myers <Tavern@Adams-Mac.local>
2017-01-27 09:32:29 -0600

    work in progress

--- javascript/message.js
+++ javascript/message.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // One IIFE should contain a function that accepts an element id, 
 // and the user message, and then add the user's message - along with the delete button - 
 // to the specified parent element. Each message should be stored in a private array in this IIFE. 
@@ -10,15 +12,38 @@ var MessageBoard = (function(originalMessageBoard) {
 
 	// 2. Event Listener for enter button grabs .value of input field
 	var inputField = document.getElementById("inputText");
-	inputField.addEventListener("keypress", function(event){
-		if (event.keycode === 13) {
-			console.log("eat");
+	inputField.addEventListener("keyup", function (event) {
+		if (event.which === 13) {
 			var messageText = inputField.value;
 			messageLog.push(messageText);
 			console.log(messageLog);
-			MessageBoard.addMessage(messageText);	
+			MessageBoard.addMessage(messageText);
 		}
 	});
+
+	// function removeMessage () {
+	// 	var counter = 0;
+	// 	var messageId = event.target.id;
+	// 	for (var u = 0; u < messageLog.length; u++) {
+	// 		if (counter === 0 && messageId === messageLog[u]) {
+	// 			messageLog.splice(u, 1);
+	// 			counter = 1;
+	// 			console.log(messageLog);
+	// 		}
+	// 	};
+	// 	event.target.parentElement.remove();
+	// };
+
+	function messageEventListener () {
+		var deleteProperty = document.getElementsByClassName("delete-me");
+
+		for (var i = 0; i < deleteProperty.length; i++) {
+			deleteProperty[i].addEventListener('click', function() {
+				var targetID = event.target.id;
+				MessageBoard.removeMessage(targetID);
+			});
+		}
+	}
 
  // Expose a function to read all messages and delete a single message
 	  return {
@@ -31,10 +56,16 @@ var MessageBoard = (function(originalMessageBoard) {
 
 	    	var deleteButton = document.createElement("BUTTON");
 	    	deleteButton.id = potatoe;
+	    	deleteButton.className = "delete-me";
 
+	    	deleteButton.innerHTML = "Delete";
 	    	createLi.appendChild(theMessage);
 	    	createLi.appendChild(deleteButton);
 	    	messageList.appendChild(createLi);
+
+	    	inputField.value = "";
+
+	    	messageEventListener();
 
 	    }
 	  };
