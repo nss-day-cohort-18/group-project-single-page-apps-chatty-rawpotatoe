f65a5d461afe88d4dee8cd57918ff8e55af69e43
Adam Myers <Tavern@adams-mac.attlocal.net>
2017-01-26 19:11:45 -0600

    Work in progress

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,9 +2,35 @@ var messageField = document.getElementById("inputText");
 var clearButton = document.getElementById("clear-board-button");
 var darkCheck = document.getElementById("dark-theme-checkbox");
 var largeCheck = document.getElementById("large-text-checkbox");
-/*
-messageField.addEventListener("keypress", submitMessage);
-clearButton.addEventListener("click", clearBoard);
-darkCheck.addEventListener("change", makeDark);
-largeCheck = document.getElementById("change", makeLarge);
-*/
+
+
+var MessageBoard = (function(originalMessageBoard) {
+
+	 var objectRequest = new XMLHttpRequest();
+      
+     objectRequest.addEventListener("load", function () {
+    	objectOfMessages = JSON.parse(event.target.responseText);
+
+    	for (var i = 0; i < objectOfMessages.length; i++) {
+    		var eachMessage = objectOfMessages[i];
+    		MessageBoard.addMessage(String(eachMessage));
+    	};
+     });
+
+     objectRequest.open("GET", "messages.json");
+     objectRequest.send();
+
+     darkCheck.addEventListener('change', function(){
+     	var bodyElement = document.getElementById("chatty-body");
+     	bodyElement.classList.toggle("dark-theme");
+     });
+
+     largeCheck.addEventListener('change', function(){
+     	var listOfMessages = document.getElementById("message-board-list");
+     	listOfMessages.classList.toggle("large-font");
+     });
+
+
+  return originalMessageBoard;
+
+})(MessageBoard || {});
