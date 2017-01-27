866b1b720c0ff65d05b8054931905f99eef8cd7f
Jordan Nelson <jnelsontn@gmail.com>
2017-01-27 13:39:19 -0600

    Added User Name Function

--- javascript/message.js
+++ javascript/message.js
@@ -8,12 +8,24 @@
 var MessageBoard = (function(originalMessageBoard) {
 
 	var messageLog = [];
+	var users = {
+  		names: ["Jordan", "Jeremy", "Adam", "Ruthie"]
+	};
 
 	var inputField = document.getElementById("inputText");
 	inputField.addEventListener("keypress", function (event) {
 		if (event.which === 13) {
 			var messageText = inputField.value;
-			MessageBoard.addMessage(messageText);
+			var userNames = document.getElementsByName("users");
+			var theUserName;
+
+			for (var k = 0; k < userNames.length; k++) {
+				if (userNames[k].checked) {
+					theUserName = userNames[k].id;
+				}
+			}
+		MessageBoard.addMessage(messageText, theUserName);	
+
 		}
 	});
 
@@ -32,16 +44,20 @@ var MessageBoard = (function(originalMessageBoard) {
 
  // Expose a function to read all messages and delete a single message
 
-	    originalMessageBoard.addMessage = function(potatoe) {
+	    originalMessageBoard.addMessage = function(potatoe, tomato) {
 	    	var messageList = document.getElementById("message-board-list");
 	    	var createLi = document.createElement("LI");
 	    	var theMessage = document.createTextNode(potatoe);
+	    	var theUser = document.createTextNode(tomato);
 
 	    	var deleteButton = document.createElement("BUTTON");
+	    	var h6tag = document.createElement("H6");
 	    	deleteButton.id = potatoe;
 	    	deleteButton.className = "delete-me";
 
 	    	deleteButton.innerHTML = "Delete";
+	    	h6tag.appendChild(theUser);
+	    	createLi.appendChild(h6tag);
 	    	createLi.appendChild(theMessage);
 	    	createLi.appendChild(deleteButton);
 	    	messageList.appendChild(createLi);
